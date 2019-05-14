fb2dd987cdebb272d5b5d275121e98386974a684
Luciano <lucianoluzzi@hotmail.com>
2019-05-13 23:35:45 -0300

    Creation of a third fragment in one the the flows and back button customization to go back to the home fragment.

--- app/src/main/java/br/com/lucianoluzzi/navigationpoc/FragmentC.kt
+++ app/src/main/java/br/com/lucianoluzzi/navigationpoc/FragmentC.kt
@@ -5,9 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_c.*
 
 class FragmentC : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_c, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val actionFragmentCToFragmentD = FragmentCDirections.actionFragmentCToFragmentD()
+        button_fragmentD.setOnClickListener(
+            Navigation.createNavigateOnClickListener(actionFragmentCToFragmentD)
+        )
     }
 }
---
+++ app/src/main/java/br/com/lucianoluzzi/navigationpoc/FragmentD.kt
@@ -0,0 +1,23 @@
+package br.com.lucianoluzzi.navigationpoc
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.activity.addCallback
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+
+class FragmentD : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requireActivity().onBackPressedDispatcher.addCallback(this) {
+            findNavController().navigate(FragmentDDirections.actionFragmentDToFragmentA())
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_d, container, false)
+    }
+}
