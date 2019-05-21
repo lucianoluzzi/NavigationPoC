a66296a8e8b1752de045134654244f48d9b3099f
Luciano <lucianoluzzi@hotmail.com>
2019-05-20 22:08:03 -0300

    bottom navigation implementation

--- app/src/main/java/br/com/lucianoluzzi/navigationpoc/MainActivity.kt
+++ app/src/main/java/br/com/lucianoluzzi/navigationpoc/MainActivity.kt
@@ -2,11 +2,15 @@ package br.com.lucianoluzzi.navigationpoc
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
+import kotlinx.android.synthetic.main.main_activity.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+        NavigationUI.setupWithNavController(bottomNavigation, nav_host_fragment.findNavController())
     }
 }
